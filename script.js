e6cd3f057809d78da86611d59db5c3ccba3a7054
JPatchara <patchara.cmu@hotmail.com>
2019-11-05 00:09:53 +0700

    [Edited] add testing file

---
+++ script.js
@@ -0,0 +1,45 @@
+const video = document.getElementById('video')
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+  faceapi.nets.ageGenderNet.loadFromUri('./models'),
+]).then(startVideo)
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(
+      video, 
+      new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withAgeAndGender().withFaceDescriptors()
+    
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+
+    resizedDetections.forEach(result => {
+      const { age, gender, genderProbability } = result
+      new faceapi.draw.DrawTextField(
+        [
+          `${faceapi.round(age, 0)} years`,
+          `${gender} (${faceapi.round(genderProbability)})`
+        ],
+        result.detection.box.bottomLeft
+      ).draw(canvas)
+    })
+  }, 100)
+})
